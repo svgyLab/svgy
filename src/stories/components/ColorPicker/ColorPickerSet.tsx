28beafa1e7676946d6574c9e4d8756c8d0894fcb
mjae404 <mjaedot@gmail.com>
2025-06-06 21:55:13 +0900

    (#2) [fix] ColorPickerSet 인스턴스 렌더링시 pickerId 값의 고유성 보장

--- src/stories/components/ColorPicker/ColorPickerSet.tsx
+++ src/stories/components/ColorPicker/ColorPickerSet.tsx
@@ -1,3 +1,4 @@
+import { useId } from 'react';
 import type { ColorPickerSetProps } from "./ColorPicker.types";
 import styles from "../../styles/components/ColorPicker/ColorPickerSet.module.scss";
 import ColorPicker from "./ColorPicker";
@@ -8,12 +9,15 @@ export default function ColorPickerSet({
     fillColor,
     fillOnChange,
   }: ColorPickerSetProps) {
+  const strokeId = useId();
+  const fillId = useId();
+
   return (
     <div className={styles["colorpicker-set"]}>
       <div className={styles["colorpicker-stroke"]}>
         <ColorPicker
           label="테두리 색상"
-          pickerId="stroke-colorpicker"
+          pickerId={strokeId}
           color={strokeColor}
           onChange={strokeOnChange}
           stroke={true}
@@ -22,7 +26,7 @@ export default function ColorPickerSet({
       <div className={styles["colorpicker-fill"]}>
         <ColorPicker
           label="채우기 색상"
-          pickerId="fill-colorpicker"
+          pickerId={fillId}
           color={fillColor}
           onChange={fillOnChange}
           stroke={false}
