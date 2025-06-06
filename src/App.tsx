38ca7e01b119dd1527e5399901b5cdb6e98fcab2
mjae404 <mjaedot@gmail.com>
2025-06-07 00:18:19 +0900

    (#4) [design] 기본 레이아웃 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { menus } from "./stories/assets/datas/datas";
+import HeaderMenuBar from "./stories/layouts/HeaderMenuBar/HeaderMenuBar";
+import Iconbar from "./stories/layouts/Iconbar/Iconbar";
+import Canvas from "./stories/layouts/Canvas/Canvas";
+import Panel from "./stories/layouts/Panel/Panel";
+import "./style.scss";
+
 function App() {
   return (
     <>
+      <HeaderMenuBar menus={menus} />
+      <main className="main">
+        <Iconbar toolbar={true} />
+        <Canvas width={300} height={300} active={true} />
+        <Iconbar toolbar={false} active={true} />
+      </main>
+      <Panel />
     </>
   );
 }
